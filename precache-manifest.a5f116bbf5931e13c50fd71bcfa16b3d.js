a71aa356ffcf79f591981b0a64f8fc7853c89558
ksooyeon <cr7070@hanmail.net>
2020-06-19 22:43:59 +0900

    Updates

---
+++ precache-manifest.a5f116bbf5931e13c50fd71bcfa16b3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cf6c801c2c0ce2c966b2fd1b478ca35",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "94dab389063e0bbd99f1",
+    "url": "/movie_app_2020/static/css/main.44832cbb.chunk.css"
+  },
+  {
+    "revision": "b2833c2e89f6cbb7d482",
+    "url": "/movie_app_2020/static/js/2.f1b02f75.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.f1b02f75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94dab389063e0bbd99f1",
+    "url": "/movie_app_2020/static/js/main.578cfc8d.chunk.js"
+  },
+  {
+    "revision": "86e4940ccaeed0f37e85",
+    "url": "/movie_app_2020/static/js/runtime-main.83f6590a.js"
+  }
+]);
